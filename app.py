4bc788d4d37d190aba8415fdc353877933922c2c
Salvador Romanelli <salvabmx@gmail.com>
2025-02-18 00:46:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'salvabmx@gmail.com'
-app.config['MAIL_PASSWORD'] = 'FIFA2004'
+app.config['MAIL_PASSWORD'] = '*********'
 app.config['MAIL_DEFAULT_SENDER'] = 'salvabmx@gmail.com'
 
 mail = Mail(app)
